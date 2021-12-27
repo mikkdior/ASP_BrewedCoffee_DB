34c9a12a661637b987a28e33d39b1eb7f203b17b
Michael <mikkdior@gmail.com>
2021-12-27 16:18:05 +0200

    Добавьте файлы проекта.

---
+++ ASP_BrewedCoffee_DB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_BrewedCoffee_DB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
